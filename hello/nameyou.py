b80a967c6ea656cef0fb3e8112e624712d4ce916
parsaCr7 AsaDev <parsacr9@gmail.com>
2020-01-15 12:49:26 +0330

    Jupyter-notebook-demo

---
+++ hello/nameyou.py
@@ -0,0 +1,94 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+x=2
+y="salam"
+x+y
+
+
+# %%
+x=3
+y="salam"
+x*y
+
+
+# %%
+x="salam"
+y="hello"
+x+y
+
+
+# %%
+x="salam"
+y="hi"
+x*y
+
+
+# %%
+x=input("لطفا یک عدد وارد کنید")
+
+
+# %%
+x=input("please enter a number")
+print(x)
+
+
+# %%
+x=input("")
+print(x)
+
+
+# %%
+x=input("name:")
+print(x)
+
+
+# %%
+x=int(input("please enter a number:"))
+x+2
+
+
+# %%
+x=int (input("pls enter a number"))
+y=int(input("pls enter a number"))
+x+y
+
+
+# %%
+x= int (input("pls enter a name"))
+x*5
+
+
+# %%
+x="zahra"
+y="parsa"
+x+y
+
+
+# %%
+x="zahra1"
+y="parsa1"
+x+y
+
+
+# %%
+x="zahra1"
+y="parsa2"
+x+y
+
+
+# %%
+x="zahra1"
+y="parsa3"
+x+y
+
+
+# %%
+x="zahra4"
+y="parsa1"
+x+y
+
+
+# %%
+
+
